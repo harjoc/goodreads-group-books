2e7f905e753106562482b99f5e4c41d31f3f602f
patraulea <patraule@gmail.com>
2017-12-28 23:54:19 +0200

    Create local_config.py

---
+++ local_config.py
@@ -0,0 +1,11 @@
+raise Exception('need api key')
+api_key = "..."
+api_secret = "..."
+
+group_id = 201118
+group_fn = 'group_filo'
+shelves_dir = 'shelves_filo'
+
+#group_id = 52853
+#group_fn = 'group_romania'
+#shelves_dir = 'shelves_romania'
