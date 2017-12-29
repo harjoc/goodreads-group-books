bb79a143ff8220cf4d5e9b2a9e65460a9c70935e
patraulea <patraule@gmail.com>
2017-12-29 19:56:54 +0200

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -5,6 +5,6 @@ api_secret = "..."
 # get this from the url of a goodreads group page
 group_id = 201118
 
-# these names are arbitrary, they will be created when running gb.py
+# these names are arbitrary, they will be created when running listbooks.py
 group_fn = 'group_filo'
 shelves_dir = 'shelves_filo'
